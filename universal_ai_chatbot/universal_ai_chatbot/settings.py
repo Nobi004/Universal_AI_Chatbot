5869512406c0c51aa67787369639be332858baa9
Mahmudun Nobi <mdmnb435@gmail.com>
2025-07-01 01:47:16 +0600

    REST_FRAMEWORK added om settomgs

--- universal_ai_chatbot/universal_ai_chatbot/settings.py
+++ universal_ai_chatbot/universal_ai_chatbot/settings.py
@@ -53,6 +53,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
 
 ROOT_URLCONF = 'universal_ai_chatbot.urls'
 
