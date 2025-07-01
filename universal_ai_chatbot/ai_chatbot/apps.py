7a95ff7cbfcd1a6a61866b48d9af9006d40b5494
Mahmudun Nobi <mdmnb435@gmail.com>
2025-07-01 06:27:05 +0600

    Update apps.py

--- universal_ai_chatbot/ai_chatbot/apps.py
+++ universal_ai_chatbot/ai_chatbot/apps.py
@@ -1,6 +1,17 @@
+# ai_chatbot/apps.py
+
 from django.apps import AppConfig
 
 
 class AiChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_chatbot'
+    verbose_name = 'AI Chatbot'
+    
+    def ready(self):
+        """Initialize app when Django starts."""
+        # Import signal handlers if any
+        try:
+            from . import signals
+        except ImportError:
+            pass
